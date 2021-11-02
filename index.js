ee1db187fb4d4dd62131cd287316162470763bef
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-11-02 15:25:44 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase();
+
+function whisper(string) {
+    return string.toLowerCase()
+}
+"HELLO!".toLowerCase();
+
+function logShout() {
+    console.log("HELLO");
+}
+
+function logWhisper() {
+    console.log("hello");
+}
+function sayHiToGrandma(string) {
+    if (string === string.toLowerCase ()) {
+        return "I can\'t hear you!";}
+    if (string === string.toUpperCase ()) {
+        return "YES INDEED!";}
+    if (string === "I love you, Grandma.") {
+        return "I love you, too.";}
+}
